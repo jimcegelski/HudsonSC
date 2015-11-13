c699da6110ce852664114a15d05970ac789cb729
Jim Cegelski <jcegelski@vloan.com>
2015-11-13 17:01:40 -0500

    Just taking a look and running under VS 2015

--- HappyHour/HappyHourTests/RegisterFixture.cs
+++ HappyHour/HappyHourTests/RegisterFixture.cs
@@ -1,11 +1,6 @@
-﻿using HappyHour;
+﻿using System.Collections.Generic;
+using HappyHour;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 
 namespace HappyHourTests
 {
