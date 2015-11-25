24a1f9ca429e01f90b0823afce37a81828991b25
Jim Cegelski <jcegelski@vloan.com>
2015-11-25 08:51:31 -0500

    Working through the RomanNumerals kata with a group of junior devs here at vLoan

---
+++ RomanNumerals/RomanNumerals/RomanNumeralConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RomanNumerals
+{
+    public class RomanNumeralConverter
+    {
+        public static string Convert(int numberToConvert)
+        {
+            var romanNumeral = string.Empty;
+            AccountForValue(10, "X", ref romanNumeral, ref numberToConvert);
+            AccountForValue(9, "IX", ref romanNumeral, ref numberToConvert);
+            AccountForValue(5, "V", ref romanNumeral, ref numberToConvert);
+            AccountForValue(4, "IV", ref romanNumeral, ref numberToConvert);
+            while (numberToConvert > 0)
+            {
+                AccountForValue(1, "I", ref romanNumeral, ref numberToConvert);
+            }
+            return romanNumeral;
+        }
+
+        private static void AccountForValue(int number, string romanNumeralRepresentation, ref string romanNumeral,
+            ref int numberToConvert)
+        {
+            if (numberToConvert < number) return;
+            romanNumeral += romanNumeralRepresentation;
+            numberToConvert = numberToConvert - number;
+        }
+
+    }
+}
---
+++ RomanNumerals/Test/RomanNumeralsTests.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using RomanNumerals;
+
+namespace Test
+{
+    public class RomanNumeralsTests
+    {
+        [Test]
+        public void One()
+        {
+            Assert.AreEqual("I", RomanNumeralConverter.Convert(1));
+        }
+
+        [Test]
+        public void Two()
+        {
+            Assert.AreEqual("II", RomanNumeralConverter.Convert(2));
+        }
+
+        [Test]
+        public void Three()
+        {
+            Assert.AreEqual("III", RomanNumeralConverter.Convert(3));
+        }
+
+        [Test]
+        public void Four()
+        {
+            Assert.AreEqual("IV", RomanNumeralConverter.Convert(4));
+        }
+
+        [Test]
+        public void Five()
+        {
+            Assert.AreEqual("V", RomanNumeralConverter.Convert(5));
+        }
+
+        [Test]
+        public void Six()
+        {
+            Assert.AreEqual("VI", RomanNumeralConverter.Convert(6));
+        }
+
+        [Test]
+        public void Seven()
+        {
+            Assert.AreEqual("VII", RomanNumeralConverter.Convert(7));
+        }
+
+        [Test]
+        public void Eight()
+        {
+            Assert.AreEqual("VIII", RomanNumeralConverter.Convert(8));
+        }
+
+        [Test]
+        public void Nine()
+        {
+            Assert.AreEqual("IX", RomanNumeralConverter.Convert(9));
+        }
+
+        [Test]
+        public void Ten()
+        {
+            Assert.AreEqual("X", RomanNumeralConverter.Convert(10));
+        }
+    }
+}
