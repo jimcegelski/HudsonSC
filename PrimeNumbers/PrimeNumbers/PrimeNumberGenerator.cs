3da97f0f7bc531e4cb2cc12e7574ad2b0e23e6ab
Jim Cegelski <jcegelski@vloan.com>
2015-11-25 14:27:43 -0500

    Added another test for 80

--- PrimeNumbers/PrimeNumbers/PrimeNumberGenerator.cs
+++ PrimeNumbers/PrimeNumbers/PrimeNumberGenerator.cs
@@ -6,21 +6,44 @@ namespace PrimeNumbers
     {
         public static int[] GetPrimeFactors(int number)
         {
-            var list = new List<int>();
+            var list = GetFactors(number);
+            return list.Count > 0 ? list.ToArray() : new[] {number};
+        }
+
+        private static List<int> GetFactors(int number)
+        {
+            var results = new List<int>();
+            for (var i = 2; i <= (number/2); i++)
+            {
+                if (number % i != 0) continue;
+
+                AddNumberToFactorsList(i, results);
+                AddNumberToFactorsList(number/i, results);
+
+                break;
+            }
+            return results;
+        }
+
+        private static void AddNumberToFactorsList(int i, List<int> results)
+        {
+            if (IsPrime(i))
+            {
+                results.Add(i);
+            }
+            else
+            {
+                results.AddRange(GetFactors(i));
+            }
+        }
+
+        private static bool IsPrime(int number)
+        {
             for (var i = 2; i < number; i++)
             {
-                if (number%i == 0)
-                {
-                    list.Add(i);
-                    if (number/i == i)
-                    {
-                        list.Add(i);
-                    }
-                }
+                if (number%i == 0) return false;
             }
-            if (list.Count > 0) return list.ToArray();
-            return new[] {number};
-
-        }
+            return true;
+        }             
     }
 }
--- PrimeNumbers/Test/Tests.cs
+++ PrimeNumbers/Test/Tests.cs
@@ -12,9 +12,33 @@ namespace Test
         }
 
         [Test]
+        public void Three()
+        {
+            Assert.AreEqual(new[] { 3 }, PrimeNumberGenerator.GetPrimeFactors(3));
+        }
+
+        [Test]
         public void Four()
         {
             Assert.AreEqual(new[] { 2, 2 }, PrimeNumberGenerator.GetPrimeFactors(4));
+        }
+
+        [Test]
+        public void Six()
+        {
+            Assert.AreEqual(new[] { 2, 3 }, PrimeNumberGenerator.GetPrimeFactors(6));
+        }
+
+        [Test]
+        public void Nine()
+        {
+            Assert.AreEqual(new[] { 3, 3 }, PrimeNumberGenerator.GetPrimeFactors(9));
+        }
+
+        [Test]
+        public void Twelve()
+        {
+            Assert.AreEqual(new[] { 2, 2, 3 }, PrimeNumberGenerator.GetPrimeFactors(12));
         }
 
         [Test]
@@ -26,7 +50,7 @@ namespace Test
         [Test]
         public void Eighty()
         {
-            Assert.AreEqual(new[] { 3, 5 }, PrimeNumberGenerator.GetPrimeFactors(80));
+            Assert.AreEqual(new[] { 2, 2, 2, 2, 5 }, PrimeNumberGenerator.GetPrimeFactors(80));
         }
     }
 }
