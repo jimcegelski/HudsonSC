55392face74ddac1c67c3949fdc7e1d58004c19e
Jim Cegelski <zilla7733@yahoo.com>
2015-12-09 14:36:24 -0500

    Working on count coins

---
+++ CountCoins/CountCoins/CoinCounter.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+
+namespace CountCoins
+{
+    public class CoinCounter
+    {
+        public static List<string> GetCoinCollections(int centsToStartWith)
+        {
+            var result = new List<string>();
+            var coinToStartWith = 10;
+            while (coinToStartWith > 0)
+            {
+                var centsRemaining = centsToStartWith;
+                var collection = new Dictionary<int, int>();
+
+                if (coinToStartWith <= centsRemaining)
+                {
+                    collection[coinToStartWith] = 1;
+                    centsRemaining = centsRemaining - coinToStartWith;
+                    AddCoinToCollection(coinToStartWith, ref centsRemaining, collection);
+                }
+
+                if (collection.Count > 0) result.Add(ConvertCollectionIntoString(collection));
+
+                coinToStartWith = GetNextLowestCoinType(coinToStartWith);
+            }
+            
+            return result;
+        }
+        
+        private static Dictionary<int, int> CreateCoinCollectionStartingWithSpecificCoin(int coinTypeToStartWith,
+            int centsToStartWith)
+        {
+            var collection = new Dictionary<int, int>();
+            if (coinTypeToStartWith <= centsToStartWith)
+            {
+                collection[coinTypeToStartWith] = 1;
+                centsToStartWith = centsToStartWith - coinTypeToStartWith;
+                AddCoinToCollection(coinTypeToStartWith, ref centsToStartWith, collection);
+            }
+
+            return collection;
+        }
+        
+        private static void AddCoinToCollection(int coinTypeToAdd, ref int centsToStartWith, Dictionary<int, int> coinCollection)
+        {
+            while (centsToStartWith > 0)
+            {
+                if (coinTypeToAdd > centsToStartWith) coinTypeToAdd = GetNextLowestCoinType(coinTypeToAdd);
+                if (!coinCollection.ContainsKey(coinTypeToAdd))
+                {
+                    coinCollection.Add(coinTypeToAdd, 0);
+                }
+                coinCollection[coinTypeToAdd] ++;
+                centsToStartWith = centsToStartWith - coinTypeToAdd;
+            }
+        }
+
+        private static string ConvertCollectionIntoString(Dictionary<int, int> coinCollection)
+        {
+            var result = string.Empty;
+            foreach (var coinType in coinCollection.Keys)
+            {
+                if (result != string.Empty) result += " and ";
+                var numberString = ConvertNumberToString(coinCollection[coinType]);
+                if (result == string.Empty)
+                {
+                    result = numberString.ToUpper();
+                }
+                else
+                {
+                    result += numberString;
+                }
+                result += " ";
+                result += ConvertCoinType(coinType, coinCollection[coinType]);
+            }
+            return result;
+        }
+
+        public static string ConvertNumberToString(int number)
+        {
+            if (number == 1) return "a";
+            return number.ToString();
+        }
+
+        public static string ConvertCoinType(int coinType, int numberOfCoins)
+        {
+            if (coinType == 1)
+            {
+                if (numberOfCoins == 1) return "penny";
+                return "pennies";
+            }
+            if (coinType == 5)
+            {
+                if (numberOfCoins == 1) return "nickel";
+                return "nickles";
+            }
+            if (coinType == 10)
+            {
+                if (numberOfCoins == 1) return "dime";
+                return "dimes";
+            }
+            return string.Empty;
+        }
+
+        public static int GetNextLowestCoinType(int coinType)
+        {
+            switch (coinType)
+            {
+                case 10:
+                    return 5;
+                case 5:
+                    return 1;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ CountCoins/Test/Test.cs
@@ -0,0 +1,53 @@
+﻿using CountCoins;
+using NUnit.Framework;
+
+namespace Test
+{
+    public class Test
+    {
+        [Test]
+        public void One()
+        {
+            var coinCollections = CoinCounter.GetCoinCollections(1);
+            Assert.AreEqual(1, coinCollections.Count);
+            Assert.AreEqual("A penny",coinCollections[0] );
+        }
+
+        [Test]
+        public void Two()
+        {
+            var coinCollections = CoinCounter.GetCoinCollections(2);
+            Assert.AreEqual(1, coinCollections.Count);
+            Assert.AreEqual("2 pennies", coinCollections[0]);
+        }
+
+        [Test]
+        public void Five()
+        {
+            var coinCollections = CoinCounter.GetCoinCollections(5);
+            Assert.AreEqual(2, coinCollections.Count);
+            Assert.AreEqual("A nickel", coinCollections[0]);
+            Assert.AreEqual("5 pennies", coinCollections[1]);
+        }
+
+        [Test]
+        public void Six()
+        {
+            var coinCollections = CoinCounter.GetCoinCollections(6);
+            Assert.AreEqual(2, coinCollections.Count);
+            Assert.AreEqual("A nickel and a penny", coinCollections[0]);
+            Assert.AreEqual("6 pennies", coinCollections[1]);
+        }
+
+        [Test]
+        public void Ten()
+        {
+            var coinCollections = CoinCounter.GetCoinCollections(10);
+            Assert.AreEqual(4, coinCollections.Count);
+            Assert.AreEqual("A dime", coinCollections[0]);
+            Assert.AreEqual("2 nickles", coinCollections[1]);
+            Assert.AreEqual("A nickel and 5 pennies", coinCollections[2]);
+            Assert.AreEqual("10 pennies", coinCollections[3]);
+        }
+    }
+}
