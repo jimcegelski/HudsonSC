5afa5a2c8c0458ef79f3e0099b6c6f9faa5c75a0
Jim Cegelski <jcegelski@vloan.com>
2016-02-04 14:26:31 -0500

    Finalized the Number Names kata

--- NumberNames/NumberNames/NumberName.cs
+++ NumberNames/NumberNames/NumberName.cs
@@ -8,34 +8,44 @@
         {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
 
         private static readonly string[] Tens = new[]
-        {"aughts", "teens", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-
+        {"aughts", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+        
         public static string GetNumberName(int numberToTest)
         {
+            return $"{HandleNumber(ref numberToTest, 1000000, " million")}" +
+                   $"{HandleNumber(ref numberToTest, 1000, " thousand")}" +
+                   $"{HandleNumber(ref numberToTest, 1, "")}";
+        }
+
+        public static string HandleNumber(ref int numberToTest, int numberToCheckFor, string numberAsString)
+        {
             var result = string.Empty;
-            if (numberToTest > 99)
-            {
-                var hundredsPlace = numberToTest/100;
-                result += Ones[hundredsPlace] + " hundred";
-                numberToTest = numberToTest - (hundredsPlace * 100);
-                if (numberToTest > 0) result += " and ";
-            }
-            if (numberToTest > 19)
-            {
-                var tensPlace = numberToTest/10;
-                result += Tens[tensPlace];
-                numberToTest = numberToTest - (tensPlace * 10);
-                if (numberToTest > 0) result += " ";
-            }
-            if (numberToTest < 10)
-            {
-                if (numberToTest > 0)
-                {
-                    result += Ones[numberToTest];
-                }
-                return result;
-            }
-            return result += Teens[numberToTest - 10];
+            if (numberToTest < numberToCheckFor) return result;
+            result = $"{GetNameForHundredsTensAndOnesPlace(numberToTest / numberToCheckFor)}{numberAsString}";
+            numberToTest = numberToTest % numberToCheckFor;
+            if (numberToTest > 0) result = $"{result}, ";
+            return result;
+        }
+
+        public static string GetNameForHundredsTensAndOnesPlace(int numberToTest)
+        {
+            var result = string.Empty;
+            ResolveOnePlace(ref numberToTest, 99, 100, " hundred", " and ", Ones, ref result);
+            ResolveOnePlace(ref numberToTest, 19, 10, "", " ", Tens, ref result);
+
+            if (numberToTest >= 10) return $"{result}{Teens[numberToTest - 10]}";
+            if (numberToTest > 0) result += Ones[numberToTest]; 
+            return result;
+        }
+
+        public static void ResolveOnePlace(ref int numberToTest, int minNumber, int divNumber, string placeIndetifier,
+            string separater, string[] arrayToUse, ref string result)
+        {
+            if (numberToTest <= minNumber) return;
+            var digitPlace = numberToTest/divNumber;
+            result = $"{result}{arrayToUse[digitPlace]}{placeIndetifier}";
+            numberToTest = numberToTest - (digitPlace*divNumber);
+            if (numberToTest > 0) result = $"{result}{separater}";
         }
     }
 }
--- NumberNames/Test/Tests.cs
+++ NumberNames/Test/Tests.cs
@@ -12,9 +12,22 @@ namespace Test
         [TestCase("nineteen", 19)]
         [TestCase("twenty", 20)]
         [TestCase("eighty nine", 89)]
+        [TestCase("ninety nine", 99)]
         [TestCase("one hundred", 100)]
         [TestCase("one hundred and one", 101)]
+        [TestCase("one hundred and eighty one", 181)]
+        [TestCase("three hundred", 300)]
+        [TestCase("three hundred and ten", 310)]
         [TestCase("five hundred and seventy three", 573)]
+        [TestCase("one thousand", 1000)]
+        [TestCase("one thousand, five hundred and one", 1501)]
+        [TestCase("one thousand, six hundred and twenty seven", 1627)]
+        [TestCase("nine thousand, nine hundred and ninety nine", 9999)]
+        [TestCase("ten thousand", 10000)]
+        [TestCase("twelve thousand, six hundred and nine", 12609)]
+        [TestCase("eighty seven thousand, three hundred and forty one", 87341)]
+        [TestCase("five hundred and twelve thousand, six hundred and seven", 512607)]
+        [TestCase("forty three million, one hundred and twelve thousand, six hundred and three", 43112603)]
         public void TestResults(string expectedResult, int numberToTest)
         {
             Assert.AreEqual(expectedResult, NumberName.GetNumberName(numberToTest));
