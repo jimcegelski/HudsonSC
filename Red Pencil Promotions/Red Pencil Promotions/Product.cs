d79684d36986c7bfead90b42caafd896c54df767
Jim <jim.cegelski@mortgagecto.com>
2017-02-17 08:51:52 -0500

    Work done on 2/15/2017 meeting

---
+++ Red Pencil Promotions/Red Pencil Promotions/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://stefanroock.wordpress.com/2011/03/04/red-pencil-code-kata/
+
+namespace Red_Pencil_Promotions
+{
+    public class Product
+    {
+        private double _currentPrice;
+        private DateTime _currentPriceEffectiveDate;
+        private bool _isRedPencil;
+
+        public bool IsRedPencil { get { return _isRedPencil && RedPencilPromotionIs30DaysOldOrLess(); } }
+
+        public void SetPrice(double newPrice, DateTime priceEffectiveDate)
+        {
+            _isRedPencil = SetIsRedPencil(newPrice, priceEffectiveDate);
+            _currentPrice = newPrice;
+            _currentPriceEffectiveDate = priceEffectiveDate;
+        }
+
+        private bool SetIsRedPencil(double newPrice, DateTime newPriceEffectiveDate)
+        {
+            if (_currentPriceEffectiveDate == DateTime.MinValue) return false;
+            if (newPrice < _currentPrice) return false;
+            
+            var diff = _currentPrice - newPrice;
+            var percentDifference = (diff / _currentPrice) * 100;
+            if (percentDifference < 5) return false;
+            if (percentDifference > 30) return false;
+
+            if (!CurrentPriceIsAtLeast30DaysOld(newPriceEffectiveDate)) return false;
+
+            return true;
+        }
+
+        private bool CurrentPriceIsAtLeast30DaysOld(DateTime newPriceEffectiveDate)
+        {
+            var span = newPriceEffectiveDate - _currentPriceEffectiveDate;
+            if (span.Days < 30) return false;
+            return true;
+        }
+
+        private bool RedPencilPromotionIs30DaysOldOrLess()
+        {
+            var span = DateTime.Now - _currentPriceEffectiveDate;
+            if (span.Days > 30) return false;
+            return true;
+        }
+    }
+}
---
+++ Red Pencil Promotions/Red Pencil Tests/ProductTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Red_Pencil_Promotions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Red_Pencil_Tests
+{
+    [TestClass]
+    public class ProductShould
+    {
+        [TestMethod]
+        public void  ReturnsRedPencilTrueWhenChangeIsOver5PercentAnd30DaysOld()
+        {
+            var product = new Product();
+            product.SetPrice(10, DateTime.Now.AddDays(-70));
+            product.SetPrice(8, DateTime.Now.AddDays(-10));
+
+            Assert.IsTrue(product.IsRedPencil);
+        }
+
+        [TestMethod]
+        public void ReturnsRedPencilFalseWhenChangeLessThan30DaysOld()
+        {
+            var product = new Product();
+            product.SetPrice(10, DateTime.Now.AddDays(-29));
+            product.SetPrice(8, DateTime.Now);
+
+            Assert.IsFalse(product.IsRedPencil);
+        }
+
+        [TestMethod]
+        public void ReturnsRedPencilFalsePriceChangeIsLessThan3Percent()
+        {
+            var product = new Product();
+            product.SetPrice(100, DateTime.Now.AddDays(-10));
+            product.SetPrice(98, DateTime.Now);
+
+            Assert.IsFalse(product.IsRedPencil);
+        }
+
+        [TestMethod]
+        public void ReturnsRedPencilFalsePriceChangeIsGreaterThan30Percent()
+        {
+            var product = new Product();
+            product.SetPrice(100, DateTime.Now.AddDays(-10));
+            product.SetPrice(69, DateTime.Now);
+
+            Assert.IsFalse(product.IsRedPencil);
+        }
+
+        [TestMethod]
+        public void ReturnsRedPencilFalseAfter30DaysOfRedPencilPromotion()
+        {
+            var product = new Product();
+            product.SetPrice(10, DateTime.Now.AddDays(-80));
+            product.SetPrice(8, DateTime.Now.AddDays(-31));
+
+            Assert.IsFalse(product.IsRedPencil);
+        }
+    }
+}
