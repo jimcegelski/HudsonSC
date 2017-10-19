976859309d2285eb0ff2d8e8979f8d4957584d32
Jim <jim.cegelski@mortgagecto.com>
2017-10-19 08:27:41 -0400

    Worked on FizzBuzz kata with Kevin on 10/18/2017

---
+++ FizzBuzz/FizzBuzz/FizzBuzz/FizzBuzzClass.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+
+namespace FizzBuzzProject
+{
+    public class FizzBuzzClass
+    {
+        public List<string> FizzBuzz()
+        {
+            var myList = new List<string>();
+            for (var i = 1; i <= 100; i++)
+            {
+                if (IsNotDivisible(i, 15, "FizzBuzz", myList) &&
+                    IsNotDivisible(i, 5, "Buzz", myList) &&
+                    IsNotDivisible(i, 3, "Fizz", myList))
+                myList.Add(i.ToString());
+            }
+            return myList;
+        }
+
+        private bool IsNotDivisible(int indexNumber,int divisor, string wordToReplaceWith, List<string> myList)
+        {
+            if (NumberIsDivisibleBy(indexNumber, divisor))
+            {
+                myList.Add(wordToReplaceWith);
+                return false;
+            }
+            return true;
+        }
+
+        private bool NumberIsDivisibleBy(int number, int divisor)
+        {
+            return (number % divisor == 0);
+        }
+
+
+    }
+}
---
+++ FizzBuzz/FizzBuzz/FizzBuzzTests/FizzBuzzShould.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzzProject;
+
+namespace FizzBuzzTests
+{
+    [TestClass]
+    public class FizzBuzzShould
+    {
+        private FizzBuzzClass _fizzBuzzClass;
+        private List<string> _testResult;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _fizzBuzzClass = new FizzBuzzClass();
+            _testResult = _fizzBuzzClass.FizzBuzz();
+        }
+
+        [TestMethod]
+        public void ReturnListOf100Strings()
+        {
+            Assert.AreEqual(100, _testResult.Count, "The result should have 100 strings");
+        }
+
+        [TestMethod]
+        public void IsNumberDivisibleByThree()
+        {
+            Assert.IsTrue(_testResult[2] == "Fizz");
+            Assert.IsTrue(_testResult[5] == "Fizz");
+            Assert.IsTrue(_testResult[8] == "Fizz");
+        }
+
+        [TestMethod]
+        public void IsNumberDisivibleByFive()
+        {
+            Assert.IsTrue(_testResult[4] == "Buzz");
+            Assert.IsTrue(_testResult[9] == "Buzz");
+            Assert.IsTrue(_testResult[19] == "Buzz");
+        }
+
+        [TestMethod]
+        public void IsNumberDivisiblebyFifteen()
+        {
+            Assert.IsTrue(_testResult[14] == "FizzBuzz");
+        }
+    }
+}
